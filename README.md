30be80775a58a601eb7237bff611a8606bc1ce65
Mikto.Vtori <gamingplayerdm@gmail.com>
2023-01-16 13:02:26 +0200

    Edited responses and fixed (#1, #2)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# discord.py-quartz
-This is our discord bot
+# quartz - Discord.py Bot
+Project is Under - Eclipse Public License - v 2.0
+
+
+### The team
+- [MitkoVtori](https://github.com/MitkoVtori)
+  - [JustRedTTG](https://github.com/JustRedTTG)
+    - [Chan-Bum](https://github.com/Chan-Bum)
+    - [dinocom33](https://github.com/dinocom33)
+  - [RadoslavTs](https://github.com/RadoslavTs)
+- [Geray114](https://github.com/Geray114)
---
+++ bot/responses.py
@@ -0,0 +1,40 @@
+
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+    if p_message == '!hello' or p_message == '<@1064172616568541227> hello':
+        return 'Hey there!'
+
+    if p_message.split()[0] == '!googlesearch':
+        return '**Processing links, wait one moment...**'
+
+    if p_message == '!help':
+        return '''
+```       
+$ Say !help for list with all commands.
+
+$ Say !hello or **@quartz hello** and quartz bot will reply you.  
+
+$ Use **!googlesearch {text}** to receive links with the most helpful information.
+
+$ Use **?** before every command, to receive private message *(direct message)*
+- Examples: ?!googlesearch *text*, ?!help, ?!hello.
+```
+'''
+
+
+def google_search(p_message):
+    try:
+
+        from googlesearch import search
+
+    except ImportError:
+
+        print("No module named 'google' found")
+
+    query = ''.join(p_message.split()[1:])
+    result = '\n'.join([j for j in search(query, tld="co.in", num=10, stop=10, pause=2)])
+    return f'''
+**`Found Results`**:
+{result}
+'''
